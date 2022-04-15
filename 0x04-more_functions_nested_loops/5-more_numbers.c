27de16ea1b604d6284da4ddf421a08a00af2dc79
steve <adjeisteve24@gmail.com>
2022-04-14 18:22:01 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+int i;
+int first_number;
+int second_number;
+int result;
+
+i = 0;
+result = 0;
+while (i < 10)
+{
+while (result <= 14)
+{
+if (result < 10)
+{
+second_number = result;
+}
+
+else
+{
+first_number = result / 10;
+
